070af0edc323b52726b8b5e48d1b188c347c2465
Zai <bluekine727@gmail.com>
2025-08-17 22:46:10 +0800

    版面調整

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 
 <template>
     <router-view/>
-   
 </template>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,21 +32,21 @@ const blogPosts = ref([
   {
     id:1,
     title:'Head to Toe Blueprints for Styling a Classic Themed Wedding',
-    date:'June 23, 2019',
-    auther:' Penci Design ',
+    date:'June 23,2019',
+    auther:'Penci Design',
     image:'https://demosoledad.pencidesign.net/soledad-wedding-planner-multipurpose/wp-content/uploads/sites/36/2019/06/2-1170x779.jpg',
   },
   {
     id:2,
     title:'The Most Fabulous & Beautiful Floral Wedding Dresses To Flaunt in 2019',
-    date:'June 23, 2019',
+    date:'June 23,2019',
     auther:'Penci Design',
     image:'https://demosoledad.pencidesign.net/soledad-wedding-planner-multipurpose/wp-content/uploads/sites/36/2019/06/3-1170x779.jpg',
   },
   {
     id:3,
     title:'How To Save Precious Wedding Happy Memories With Wedding Photos',
-    date:'June 23, 2019',
+    date:'June 23,2019',
     auther:'Penci Design',
     image:'https://demosoledad.pencidesign.net/soledad-wedding-planner-multipurpose/wp-content/uploads/sites/36/2019/06/6-1170x780.jpg',
   },
@@ -164,13 +164,13 @@ onMounted(() => {
 
       <!-- 文字內容 -->
       <div class="w-full md:w-4/12 space-y-2 lg:text-left">
-        <h3 class="text-l font-semibold text-cyan-500 uppercase [letter-spacing:6px]">about us</h3>
+        <h3 class="text-sm font-black text-cyan-500 uppercase [letter-spacing:6px]">about us</h3>
         <h2 class="text-2xl font-serif">Wedding & Events</h2>
         <p class="text-gray-700 leading-relaxed text-left">
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor em que laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae, penci.
         </p>
-        <button class="inline-block bg-cyan-500 hover:bg-cyan-600 text-white font-bold
-                       py-3 px-6 rounded-3xl transition">
+        <button class="inline-block bg-cyan-500  text-white hover:bg-gray-100 hover:text-cyan-500 font-bold
+                       py-4 px-10 rounded-full transition duration-300">
           Call Us Now
         </button>
       </div>
@@ -180,7 +180,7 @@ onMounted(() => {
   <section class="py-24 bg-gray-100">
     <!-- 標題區 -->
     <div class="text-center space-y-3 mb-10">
-        <h3 class="text-l font-semibold text-cyan-500 uppercase [letter-spacing:6px]">wedding planner</h3>
+        <h3 class="text-sm font-black text-cyan-500 uppercase [letter-spacing:6px]">wedding planner</h3>
         <h2 class="text-2xl font-serif">Wedding Services</h2>
         <p class="text-gray-700 leading-relaxed w-full md:w-3/5 lg:w-1/3 mx-auto">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor em que, viva la vida penci wed.</p>
     </div>
@@ -219,7 +219,7 @@ onMounted(() => {
   <!-- 顧客感言 -->
   <section class="py-24 bg-white">
       <div class="text-center space-y-3">
-          <h3 class="text-l font-semibold text-cyan-500 uppercase [letter-spacing:6px]">wedding planner</h3>
+          <h3 class="text-sm font-black text-cyan-500 uppercase [letter-spacing:6px]">wedding planner</h3>
           <h2 class="text-2xl font-serif">What Customers Say</h2>
       </div>
       <div class="max-w-6xl mx-auto px-4">
@@ -258,8 +258,8 @@ onMounted(() => {
           <div class="text-center text-white w-4/5  space-y-3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <h2 class="text-2xl font-serif">Elegant Wedding</h2>
             <p class="text-white leading-relaxed ">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor em que, viva la vida penci wed.</p>
-            <button class="inline-block bg-cyan-500 hover:bg-cyan-600 text-white font-bold
-                       py-3 px-6 rounded-3xl transition">Call Us Now</button>
+            <button class="inline-block bg-cyan-500  text-white hover:bg-gray-100 hover:text-cyan-500 font-bold py-4 px-10 rounded-full transition duration-300">Call Us Now</button>
+                       
           </div>
         </div>
         <!-- 右圖 -->
@@ -290,7 +290,7 @@ onMounted(() => {
     <div class="max-w-6xl mx-auto space-y-9">
       <!-- 標題 -->
       <div class="text-center space-y-3">
-        <h3 class="text-l font-semibold text-cyan-500 uppercase [letter-spacing:6px]">wedding planner</h3>
+        <h3 class="text-sm font-black text-cyan-500 uppercase [letter-spacing:6px]">wedding planner</h3>
         <h2 class="text-2xl font-serif">Our Wedding Blog</h2>
         <p class="text-gray-700 leading-relaxed md:w-1/2 w-4/5 mx-auto">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor em que, viva la vida penci wed.</p>
       </div>
@@ -299,11 +299,12 @@ onMounted(() => {
           <img 
             :src="blogPost.image" 
             alt="Our Wedding Blog"
-            class=""
+            class="hover:opacity-75 transition duration-200 cursor-pointer"
+            :title="blogPost.title"
           >
-          <h3 class="font-bold">{{ blogPost.title }}</h3>
+          <h3 class="font-bold hover:opacity-50 transition duration-300 cursor-pointer">{{ blogPost.title }}</h3>
           <div class="text-xs text-gray-700 ">
-            by <a class="font-semibold">{{ blogPost.auther }}</a> | <span>{{ blogPost.date }}</span>
+            <pre>by <a class="font-semibold">{{ blogPost.auther }}</a> | <span>{{ blogPost.date }}</span></pre>
           </div>
         </div>
       </div>
@@ -311,31 +312,99 @@ onMounted(() => {
   </section>
   <!-- send us -->
   <section>
-    <div class="">
-      <div class="relative min-h-[500px]">
-        <img src="https://demosoledad.pencidesign.net/soledad-wedding-planner-multipurpose/wp-content/uploads/sites/36/2019/11/7-1170x780.jpg" alt="send us"class="absolute inset-0 w-full h-full object-cover">
-        <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
-          <!-- 標題 -->
-          <div> 
-            <h3>send us</h3>
-            <h2>Contact Info</h2>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor em que, viva la vida penci wed.</p>
-          </div>
+    <div class="flex flex-col md:flex-row h-full" >
+      <!-- 左邊 -->
+      <div class=" w-full md:flex-1 ">
+        <div class="relative min-h-[500px] overlay-container">
+          <img src="https://demosoledad.pencidesign.net/soledad-wedding-planner-multipurpose/wp-content/uploads/sites/36/2019/11/7-1170x780.jpg" alt="send us"class="absolute inset-0 w-full h-full object-cover">
           <!--  -->
-          <div class="text-white flex flex-col">
+          <div class="w-5/6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white space-y-4">
+            <!-- 標題 -->
+            <div class="space-y-4"> 
+              <h3 class="text-sm font-black uppercase [letter-spacing:6px]">send us</h3>
+              <h2 class="text-2xl font-serif">Contact Info</h2>
+              <p class="leading-relaxed text-left">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor em que, viva la vida penci wed.</p>
+            </div>
+            <!-- icon -->
+            <div class="space-y-3">
+              <div class="text-white flex justify-between items-center space-x-4">
+                <div class="flex items-center space-x-2">
+                  <!-- fas 是 solid 風格 -->
+                  <font-awesome-icon :icon="['fas','map-pin']" class="text-white"/>
+                  <h3 class="text-lg font-bold">Address</h3>
+                </div>  
+                <div class="flex-grow border-b border-white border-dashed"></div>
+                <p class="">Central Park, NYC</p>
+              </div>
+              <div class="text-white flex justify-between items-center space-x-4">
+                <div class="flex items-center space-x-2">
+                  <font-awesome-icon :icon="['fas','envelope']" class="text-white"/>
+                  <h3 class="text-lg font-bold">Email Address</h3>
+                </div>  
+                <div class="flex-grow border-b border-white border-dashed"></div>
+                <p class="">pencidesign@gmail.com</p>
+              </div>
+              <div class="text-white flex justify-between items-center space-x-4">
+                <div class="flex items-center space-x-2">
+                  <font-awesome-icon :icon="['fas','phone-alt']" class="text-white"/>
+                  <h3 class="text-lg font-bold">Phone Number</h3>
+                </div>  
+                <div class="flex-grow border-b border-white border-dashed"></div>
+                <p class="">+(1800)9388</p>
+              </div>
+            </div> 
+          </div>
+        
+        </div>
+      </div>
+      <!-- 中間 -->
+      <div class="w-full min-h-[500px] md:flex-1 bg-gray-100 flex items-center justify-center">
+        <div class="w-5/6 space-y-4">
+          <!-- 標題 -->
+          <div class="space-y-4">
+            <h3 class="text-sm text-cyan-500 font-black uppercase [letter-spacing:6px]">wedding planner</h3>
+            <h2 class="text-2xl font-serif">Opening Hours</h2>
+            <p class="leading-relaxed text-gray-600">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor em que, viva la vida penci wed.</p>
+          </div>
+          <!-- icon -->
+          <div class="space-y-3">
             <div class="flex items-center space-x-4">
-              <font-awesome-icon :icon="['fas','map-marker-alt']" class="text-white"/>
-              <h3 class="text-lg font-bold">Email Address</h3>
-              <div class="flex-1 border-b border-white border-dashed"></div>
-            </div>  
-            <p class="">Central Park, NYC</p>
+              <div class="flex items-center space-x-2">
+                <font-awesome-icon :icon="['fas','alarm-clock']" class="text-cyan-500" />
+                <h3 class="text-lg font-bold text-gray-600">Mon - Friday</h3>
+              </div>
+              <div class="flex-1 border border-b border-gray-400 border-dashed"></div>
+              <p class="text-gray-600">8:00 AM - 9:00 PM</p>
+            </div>
+             <div class="flex items-center space-x-4">
+              <div class="flex items-center space-x-2">
+                <font-awesome-icon :icon="['fas','alarm-clock']" class="text-cyan-500" />
+                <h3 class="text-lg font-bold text-gray-600">Saturday</h3>
+              </div>
+              <div class="flex-1 border border-b border-gray-400 border-dashed"></div>
+              <p class="text-gray-600">8:00 AM - 9:00 PM</p>
+            </div>
+             <div class="flex items-center space-x-4">
+              <div class="flex items-center space-x-2">
+                <font-awesome-icon :icon="['fas','alarm-clock']" class="text-cyan-500" />
+                <h3 class="text-lg font-bold text-gray-600">Sunday & Holiday</h3>
+              </div>
+              <div class="flex-1 border border-b border-gray-400 border-dashed"></div>
+              <p class="text-gray-600">8:00 AM - 9:00 PM</p>
+            </div>
           </div>
         </div>
-        
+      </div>
+      <!-- 右邊 -->
+      <div class="w-full md:flex-1 min-h[500px]">
+        <iframe 
+          src="https://www.youtube.com/embed/XHOmBV4js_E?si=Jy3ZeWcUGyRm3NgX" 
+          allowfullscreen
+          class="w-full h-full"
+        >
+        </iframe>
       </div>
     </div>
-    <div></div>
-    <div></div>
   </section>
 </template>
 
